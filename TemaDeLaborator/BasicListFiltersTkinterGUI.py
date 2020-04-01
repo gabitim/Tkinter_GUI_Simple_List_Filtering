15688876d1df358ecbf4470bee10b3a344e36d41
Timofti Gabriel <gabitim470@gmail.com>
2020-04-01 20:10:57 +0300

    big problem: cannot start process twice!, neither terminate it works to clear the entry text box after get list works to use the queue and print to the console NOT yet print in the result box

--- TemaDeLaborator/BasicListFiltersTkinterGUI.py
+++ TemaDeLaborator/BasicListFiltersTkinterGUI.py
@@ -2,31 +2,64 @@ import tkinter as tk
 from multiprocessing import Process, Queue
 from collections import deque
 
-# output in result field
-result = ""
 
 
-def filter_odd(list_queue):
-    elem = list_queue.get()
-    print(elem[1])
+# function witch selects the respective process
+# and calls the printing function(the printing in the result box doesnt work right now)
+def select_option(select):
+    if select == 0:
+        process0.start()
+    elif select == 1:
+        process1.start()
+    elif select == 2:
+        process2.start()
 
-def filter_primes(list_queue):
-    pass
+    print_result()
 
-def sum_numbers(list_queue):
-    pass
+# filter odd process (test)
+def filter_odd(list_queue,result):
+    if not list_queue.empty():
+        elem = list_queue.get()
+        print(elem[0])
+        result += elem[0]
+        print(result)
+    else:
+        result = "INSERT NUMBERS"
+
+# filter primes process (test)
+def filter_primes(list_queue, result):
+    if not list_queue.empty():
+        elem = list_queue.get()
+        print(elem[1])
+        result = elem[1]
+    else:
+        result = "INSERT NUMBERS"
+
+# sum numbers process (test)
+def sum_numbers(list_queue, result):
+    if not list_queue.empty():
+        elem = list_queue.get()
+        print(elem[2])
+        result = elem[2]
+    else:
+        result = "INSERT NUMBERS"
 
 
 # the message queue used for sending the lists
 list_queue = Queue()
 
-process0 = Process(target=filter_odd, args=(list_queue,))
-process1 = Process(target=filter_primes, args=(list_queue,))
-process2 = Process(target=sum_numbers, args=(list_queue,))
+# output in result field (not good!!, doesnt work to print from process)
+result = "asc"
+
+# declaring the processes
+process0 = Process(target=filter_odd, args=(list_queue,result))
+process1 = Process(target=filter_primes, args=(list_queue,result))
+process2 = Process(target=sum_numbers, args=(list_queue,result))
 
 # function to save input and transforms the input into a list
 def save_input():
     input_value = array_list_entry.get("1.0", "end-1c")
+    array_list_entry.delete('1.0',tk.END)
     list = input_value.split(',')
 
     list_queue.put(list)
@@ -34,8 +67,7 @@ def save_input():
 
 # function which prints in result field
 def print_result():
-    var = list_queue.get()
-    result_field.set(var[1])
+    result_field.set(result)
 
 # noinspection PyInterpreter
 if __name__ == "__main__":
@@ -58,6 +90,7 @@ if __name__ == "__main__":
     canvas.itemconfig(label, text="List of Integers \n add numbers with \n ',' between them")
 
     # the entry field
+    empty_the_entry_field = tk.StringVar()
     array_list_entry = tk.Text(window, height = 1, width=50)
     array_list_entry.pack(pady=5)
 
@@ -75,17 +108,17 @@ if __name__ == "__main__":
 
     # the filter odd button
     Button_FilterOdd = tk.Button(window, text="Filter Odd", bg='gray', height=1, width=10,
-                                 command=lambda: process0.start())
+                                 command=lambda: select_option(0))
     Button_FilterOdd.place(x=550, y=50)
 
     # the filter Primes button
     Button_FilterPrimes = tk.Button(window, text="Filter Primes", bg='gray', height=1, width=10,
-                                 command=lambda: process1.start())
+                                 command=lambda: select_option(1))
     Button_FilterPrimes.place(x=550, y=80)
 
     # the filter odd button
     Button_Sum = tk.Button(window, text="Sum numbers", bg='gray', height=1, width=10,
-                                 command=lambda: process2.start())
+                                 command=lambda: select_option(2))
     Button_Sum.place(x=550, y=110)
 
 
