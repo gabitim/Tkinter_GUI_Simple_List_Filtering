6c75e9bb0a9a31d7ed3023702da01c8211074067
Timofti Gabriel <gabitim470@gmail.com>
2020-04-01 18:13:30 +0300

    Process initialization; Message Queue test; Read from entry in a function, print to console from another function

--- TemaDeLaborator/BasicListFiltersTkinterGUI.py
+++ TemaDeLaborator/BasicListFiltersTkinterGUI.py
@@ -1,17 +1,41 @@
 import tkinter as tk
+from multiprocessing import Process, Queue
+from collections import deque
 
-global_expres_test = ""
+# output in result field
+result = ""
 
+
+def filter_odd(list_queue):
+    elem = list_queue.get()
+    print(elem[1])
+
+def filter_primes(list_queue):
+    pass
+
+def sum_numbers(list_queue):
+    pass
+
+
+# the message queue used for sending the lists
+list_queue = Queue()
+
+process0 = Process(target=filter_odd, args=(list_queue,))
+process1 = Process(target=filter_primes, args=(list_queue,))
+process2 = Process(target=sum_numbers, args=(list_queue,))
+
+# function to save input and transforms the input into a list
 def save_input():
-    global global_expres_test
-
     input_value = array_list_entry.get("1.0", "end-1c")
+    list = input_value.split(',')
 
-    global_expres_test += input_value
+    list_queue.put(list)
+    # print_result()
 
-def print_result(var):
-    result_field.set(var)
-
+# function which prints in result field
+def print_result():
+    var = list_queue.get()
+    result_field.set(var[1])
 
 # noinspection PyInterpreter
 if __name__ == "__main__":
@@ -28,14 +52,12 @@ if __name__ == "__main__":
     window.geometry("640x300")
 
     # "List of Integers" label
-    canvas = tk.Canvas(window, borderwidth=0, highlightthickness=0, bg="gray", width=100, height=20)
+    canvas = tk.Canvas(window, borderwidth=0, highlightthickness=0, bg="gray", width=110, height=60)
     canvas.place(x=0, y=0)
     label = canvas.create_text(5, 5, anchor="nw")
-    canvas.itemconfig(label, text="List of Integers")
+    canvas.itemconfig(label, text="List of Integers \n add numbers with \n ',' between them")
 
     # the entry field
-    # array_list = tk.StringVar()
-    # array_list.set('')
     array_list_entry = tk.Text(window, height = 1, width=50)
     array_list_entry.pack(pady=5)
 
@@ -53,18 +75,20 @@ if __name__ == "__main__":
 
     # the filter odd button
     Button_FilterOdd = tk.Button(window, text="Filter Odd", bg='gray', height=1, width=10,
-                                 command=lambda: print_result(global_expres_test))
+                                 command=lambda: process0.start())
     Button_FilterOdd.place(x=550, y=50)
 
     # the filter Primes button
     Button_FilterPrimes = tk.Button(window, text="Filter Primes", bg='gray', height=1, width=10,
-                                 command=lambda: print_result('ccc'))
+                                 command=lambda: process1.start())
     Button_FilterPrimes.place(x=550, y=80)
 
     # the filter odd button
     Button_Sum = tk.Button(window, text="Sum numbers", bg='gray', height=1, width=10,
-                                 command=lambda: print_result('ddd'))
+                                 command=lambda: process2.start())
     Button_Sum.place(x=550, y=110)
+
+
 
     # main loop
     window.mainloop()
